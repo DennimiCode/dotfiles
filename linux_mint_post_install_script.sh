c476d2a3b4f9d544389b897a353b17f7b619f784
DennimiCode <dennimi@duck.com>
2024-05-16 21:17:10 +0300

    Updated program versions. Teams for linux changed from flatpak to native deb repo. Changed golang from native package to bin. Added script for creating desktop files for appimages.

--- linux_mint_post_install_script.sh
+++ linux_mint_post_install_script.sh
@@ -56,6 +56,11 @@ curl -fsSL https://repo.skype.com/data/SKYPE-GPG-KEY | sudo gpg --dearmor | \
 echo deb [arch=$PC_ARCH signed-by=/usr/share/keyrings/skype.gpg] https://repo.skype.com/deb stable main | \
   sudo tee /etc/apt/sources.list.d/skype.list
 
+# Teams for linux
+sudo wget -qO /etc/apt/keyrings/teams-for-linux.asc https://repo.teamsforlinux.de/teams-for-linux.asc
+echo "deb [arch=$PC_ARCH signed-by=/etc/apt/keyrings/teams-for-linux.asc] https://repo.teamsforlinux.de/debian/ stable main" | \
+  sudo tee /etc/apt/sources.list.d/teams-for-linux-packages.list
+
 # Microsoft SQL Server
 curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
@@ -78,28 +83,29 @@ sudo apt update
 
 sudo apt install -y zsh code sqlitebrowser obs-studio gimp inkscape skypeforlinux signal-desktop qbittorrent \
   mssql-server mongodb-org postgresql postgresql-contrib mysql-server mysql-client dotnet8 dotnet6 \
-  gdebi gcc g++ clangd clang python3 python3-pip python3-venv golang nodejs git build-essential \
+  gdebi gcc g++ clangd clang python3 python3-pip python3-venv nodejs git build-essential \
   libssl-dev libffi-dev python3-dev qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager \
-  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin yaru-theme*
+  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin teams-for-linux yaru-theme*
 
 # Install & upply papirus folders
 wget -qO- https://git.io/papirus-folders-install | sh
-papirus-folders -C green --theme Papirus-Dark
+papirus-folders -C teal --theme Papirus-Dark
 
 # Install zshrc config
-sudo mv dotfiles/.zshrc /home/$USER_NAME/
+sudo cp -f dotfiles/.zshrc /home/$USER_NAME/
 
 # Download DEB apps
 wget 'https://desktop.docker.com/linux/main/amd64/docker-desktop-4.26.1-amd64.deb' -O docker-desktop.deb
 wget 'https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb' -O jdk-21.deb
-wget 'https://storage.googleapis.com/dbvis-download/product_download/dbvis-24.1/media/dbvis_linux_24_1.deb' -O dbvis.deb
-wget 'https://downloads.mongodb.com/compass/mongodb-compass_1.41.0_amd64.deb' -O mongodb-compass.deb
+wget 'https://desktop.docker.com/linux/main/amd64/149282/docker-desktop-4.30.0-amd64.deb' -O dbvis.deb
+wget 'https://downloads.mongodb.com/compass/mongodb-compass_1.43.0_amd64.deb' -O mongodb-compass.deb
 wget 'https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb' -O onlyoffice.deb
-wget 'https://github.com/localsend/localsend/releases/download/v1.13.1/LocalSend-1.13.1-linux-x86-64.deb' -O LocalSend.deb
-wget 'https://dl.discordapp.net/apps/linux/0.0.40/discord-0.0.40.deb' -O discord.deb
-wget 'https://anytype-release.fra1.cdn.digitaloceanspaces.com/anytype_0.37.3_amd64.deb' -O anytype.deb
-wget 'https://cdn.zoom.us/prod/5.17.1.1840/zoom_amd64.deb' -O zoom.deb
+wget 'https://github.com/localsend/localsend/releases/download/v1.14.0/LocalSend-1.14.0-linux-x86-64.deb' -O LocalSend.deb
+wget 'https://dl.discordapp.net/apps/linux/0.0.54/discord-0.0.54.deb' -O discord.deb
+wget 'https://anytype-release.fra1.cdn.digitaloceanspaces.com/anytype_0.40.8_amd64.deb' -O anytype.deb
+wget 'https://cdn.zoom.us/prod/6.0.2.4680/zoom_amd64.deb' -O zoom.deb
 wget 'https://download2.gluonhq.com/scenebuilder/21.0.0/install/linux/SceneBuilder-21.0.0.deb' -O SceneBuilder.deb
+wget 'https://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community_8.0.36-1ubuntu22.04_amd64.deb' -O mysql-workbench-community.deb
 
 # Download tar.gz apps
 wget 'https://download-cdn.jetbrains.com/datagrip/datagrip-2023.3.3.tar.gz' -O jetbrains-datagrip.tar.gz
@@ -109,6 +115,7 @@ wget 'https://eclipse.mirror.garr.it/oomph/epp/2023-12/R/eclipse-inst-jre-linux6
 wget 'https://td.telegram.org/tlinux/tsetup.4.16.8.tar.xz' -O telegram.tar.xz
 wget 'https://dl.pstmn.io/download/latest/linux_64' -O postman.tar.gz
 wget 'https://github.com/ful1e5/apple_cursor/releases/download/v2.0.0/macOS-Monterey.tar.gz' -O macOS-Monterey.tar.gz
+wget 'https://dl.google.com/go/go1.22.3.linux-amd64.tar.gz' -O golang.tar.gz
 
 # Install downloaded DEB apps
 sudo apt install -y ./docker-desktop.deb
@@ -121,10 +128,10 @@ sudo apt install -y ./discord.deb
 sudo apt install -y ./anytype.deb
 sudo apt install -y ./zoom.deb
 sudo apt install -y ./SceneBuilder.deb
+sudo apt install -y ./mysql-workbench-community.deb
 
 # Install flatpak apps
 flatpak install -y flathub com.github.eneshecan.WhatsAppForLinux
-flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
 flatpak install -y flathub com.usebottles.bottles
 
 # Install tar.gz apps
@@ -134,6 +141,8 @@ sudo tar -vzxf jetbrains-rider.tar.gz -C /opt/
 sudo tar -vxf telegram.tar.xz -C /opt/
 sudo tar -vzxf postman.tar.gz -C /opt/
 sudo tar -vzxf macOS-Monterey.tar.gz -C /usr/share/icons/
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -vxzf golang.tar.gz
 tar -vzxf eclipse.tar.gz
 
 # Download & install AppImages
@@ -148,6 +157,35 @@ chmod u+x bitwarden.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 sudo mv krita.appimage /opt/
 sudo mv bitwarden.appimage /opt/
+
+# Add menu entries for appimage programs
+touch /home/dennimi/.local/share/applications/Bitwarden.desktop
+touch /home/dennimi/.local/share/applications/Krita.desktop
+echo \
+'[Desktop Entry]
+Type=Application
+Name=Bitwarden
+Icon=bitwarden
+Comment=The password manager trusted by millions
+Exec=/opt/bitwarden.appimage
+Encoding=UTF-8
+Terminal=false
+Categories=Utility;
+' > /home/dennimi/.local/share/applications/Bitwarden.desktop
+echo \
+'
+[Desktop Entry]
+Type=Application
+Name=Krita
+Icon=krita
+Comment=Professional FREE and open source painting program.
+Exec=/opt/krita.appimage
+Encoding=UTF-8
+Terminal=false
+Categories=Graphics;
+' > /home/dennimi/.local/share/applications/Krita.desktop
+chmod +x /home/dennimi/.local/share/applications/Bitwarden.desktop
+chmod +x /home/dennimi/.local/share/applications/Krita.desktop
 
 # Install neovim config
 sudo mv dotfiles/nvim/ /home/$USER_NAME/.config/nvim/
@@ -170,6 +208,7 @@ rm telegram.tar.xz
 rm postman.tar.gz
 rm eclipse.tar.gz
 rm macOS-Monterey.tar.gz
+rm golang.tar.gz
 
 rm -rf mysql-workbench-dark-theme/
 
@@ -183,6 +222,7 @@ rm LocalSend.deb
 rm discord.deb
 rm anytype.deb
 rm zoom.deb
+rm mysql-workbench-community.deb
 
 # Start installed services
 sudo systemctl start mongod
@@ -191,3 +231,4 @@ sudo systemctl status mongod
 
 chmod u+x eclipse-installer/eclipse-inst
 eclipse-installer/eclipse-inst
+sudo rm -rf eclipse-installer
