ecd31348a4ee13fc6f1aa1897de76b571f2ce0f1
DennimiCode <dennimi@duck.com>
2024-06-27 14:28:26 +0300

    Added aliases for PowerShell new & Windows PowerShell

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,51 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/easy-term.omp.json" | Invok
 # for `choco` will not function.
 # See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
+if (Test-Path($ChocolateyProfile))
+{
   Import-Module "$ChocolateyProfile"
+}
+
+New-Alias vim nvim
+New-Alias vi nvim
+New-Alias g git
+function gst
+{
+  git status
+}
+function wu
+{
+  winget update
+}
+function wid([string]$softwareId)
+{
+  winget install --id $softwareId
+}
+function wi
+{
+  winget install
+}
+function wfu
+{
+  winget upgrade --all
+}
+function wiu([string]$softwareId)
+{
+  winget upgrade --id $softwareId
+}
+function poweroff
+{
+  shutdown /s
+}
+function reboot
+{
+  shutdown /r
+}
+function gcl([string]$repoUrl)
+{
+  git clone $repoUrl
+}
+function ga([string]$file)
+{
+  git add $file
 }
--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,44 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/easy-term.omp.json" | Invoke-Expression
+New-Alias vim nvim
+New-Alias vi nvim
+New-Alias g git
+function gst
+{
+  git status
+}
+function wu
+{
+  winget update
+}
+function wid([string]$softwareId)
+{
+  winget install --id $softwareId
+}
+function wi
+{
+  winget install
+}
+function wfu
+{
+  winget upgrade --all
+}
+function wiu([string]$softwareId)
+{
+  winget upgrade --id $softwareId
+}
+function poweroff
+{
+  shutdown /s
+}
+function reboot
+{
+  shutdown /r
+}
+function gcl([string]$repoUrl)
+{
+  git clone $repoUrl
+}
+function ga([string]$file)
+{
+  git add $file
+}
