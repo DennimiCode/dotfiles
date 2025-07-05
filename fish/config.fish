70a6beb06eb87426a37d8c4067cb6748e12102d5
DennimiCode <d.r.denisov.official@gmail.com>
2025-07-05 19:33:12 +0300

    feat(fish): add fish shell config
    Add config.fish - main fish config.
    Add fish_plugins - file with plugins for fisher.

---
+++ fish/config.fish
@@ -0,0 +1,37 @@
+if not test -f ~/.config/fish/functions/fisher.fish
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish \
+        | source && fisher install jorgebucaran/fisher
+
+    tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='12-hour format' \
+        --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat \
+        --powerline_prompt_style='Two lines, frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No \
+        --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Compact --icons='Many icons' --transient=No
+end
+
+if status is-interactive
+    set fish_greeting
+end
+
+alias ls="lsd"
+
+alias nv="nvim"
+alias vi="nvim"
+alias vim="nvim"
+
+alias dnfupd="sudo dnf update --refresh"
+alias dnfi="sudo dnf install"
+alias dnfr="sudo dnf remove"
+alias dnfca="sudo dnf clean all"
+alias full-upgrade="sudo dnf update --refresh -y; flatpak update -y"
+
+alias fu="flatpak update"
+alias fin="flatpak install"
+
+alias cddesk="cd ~/Desktop/"
+alias cddown="cd ~/Downloads/"
+
+alias tmux="tmux -2"
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx SUDO_EDITOR nvim
